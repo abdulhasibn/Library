ad7bb0eac6a9c23103f8d3a373d52d886e85565e
Abdul Hasib <abdulhasibn@gmail.com>
2022-09-19 22:35:52 +0530

    Change the event listener
    initially the event the add button listened for was 'click' and  due to
    that form validation was not possible. Now the event the submit button
    listens for is 'submit' so that form validation is done and refreshing
    the page issue is fixed.

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  const book = document.getElementById("book").value;
-  const author = document.getElementById("author").value;
-  const pageNumber = document.getElementById("page_no.").value;
+  const book = document.getElementById("book_name").value;
+  const author = document.getElementById("author_name").value;
+  const pageNumber = document.getElementById("page_number").value;
   const read = document.getElementById("read_status").value;
   myLibrary.push(new Book(book, author, pageNumber, read));
 }
@@ -21,13 +21,14 @@ function displayBook() {
   let bookName = document.createElement("p");
   let authorName = document.createElement("p");
   let numberOfPages = document.createElement("p");
-  let readContainer = document.createElement("div");
-  readContainer.className = "read_container";
+  let readStatusContainer = document.createElement("div");
+  readStatusContainer.className = "read_status_container";
   let readStatus = document.createElement("p");
-  readContainer.appendChild(readStatus);
+  readStatusContainer.appendChild(readStatus);
   let readToggle = document.createElement("input");
   readToggle.type = "checkbox";
-  readContainer.appendChild(readToggle);
+  readToggle.className = "read_toggle_btn";
+  readStatusContainer.appendChild(readToggle);
   bookName.textContent = `Book: ${myLibrary[myLibrary.length - 1].title}`;
   authorName.textContent = `Author: ${myLibrary[myLibrary.length - 1].author}`;
   numberOfPages.textContent = `Number of Pages: ${
@@ -37,7 +38,7 @@ function displayBook() {
   cardDiv.appendChild(bookName);
   cardDiv.appendChild(authorName);
   cardDiv.appendChild(numberOfPages);
-  cardDiv.appendChild(readStatus);
+  cardDiv.appendChild(readStatusContainer);
   const trash = document.createElement("div");
   trash.className = "trash_icon_container";
   const trashImg = `<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAALVJREFUSEtjZKAxYKSx+QwELWiPikpgZGScj80h////T6xctmwBPkfitaArKsrgHyPjeXwGMP3/b1i2bNkFXGpQLOiIjv5PjSCrWLoUbi59LaCG69HNwBoH5AYVctDALBq1ACXIR4OIYCoeDaLRIEKEAM3Lovbo6AeMDAzyBAMdVcHFiqVLDYgqTTtjYgL+///fwMDAoE+MJf8ZGB4yMTIWlC9ZsoEoC4gxlFg1BCt9Yg3CpQ4AR+xbGXAlTJ4AAAAASUVORK5CYII=" alt="delete" class="trash_icon"/>`;
@@ -58,11 +59,13 @@ addNew.addEventListener("click", () => {
   const modal = document.getElementById("modal");
   modal.showModal();
 });
-const addBtn = document.getElementById("close_btn");
-addBtn.addEventListener("click", () => {
+
+const form = document.getElementById("my_form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   addBookToLibrary();
-  modal.close();
   displayBook();
+  modal.close();
 });
 const closeBtn = document.getElementById("x");
 closeBtn.addEventListener("click", () => {
